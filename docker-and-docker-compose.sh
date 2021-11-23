e11d18c762464dc4486a3a12424ecd3c990a92c1
Lukas <w4ts0n@noreply.codeberg.org>
2021-11-23 14:09:26 +0100

    Update 'docker-and-docker-compose.sh'

--- docker-and-docker-compose.sh
+++ docker-and-docker-compose.sh
@@ -9,7 +9,7 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
     $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt update -y && apt upgrade -y && apt dist-upgrade -y && apt autoremove -y && apt autoclean
-apt-get install docker-ce docker-ce-cli containerd.io
+apt-get install docker-ce docker-ce-cli containerd.io -y
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
