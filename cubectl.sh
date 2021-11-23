d90ccbca36f6be59c962e1da29320de0daf20486
Lukas <w4ts0n@noreply.codeberg.org>
2021-11-23 20:24:54 +0100

    Update 'cubectl.sh'

--- cubectl.sh
+++ cubectl.sh
@@ -1,5 +1,8 @@
  #!/bin/bash 
-mkdir -p .kube
-curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.22.0/bin/linux/amd64/kubectl
-chmod +x ./kubectl
-sudo mv ./kubectl /usr/local/bin/kubectl
+#mkdir -p .kube
+#curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.22.0/bin/linux/amd64/kubectl
+#chmod +x ./kubectl
+#sudo mv ./kubectl /usr/local/bin/kubectl
+
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube
